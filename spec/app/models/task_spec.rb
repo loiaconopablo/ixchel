68c25889ff0924a7d947318f4e1ae3e0cf7f99f4
sairaponce <saira.ponce.ici@gmail.com>
2013-11-10 21:22:14 +0100

    Haciendo spec a task model

--- spec/app/models/task_spec.rb
+++ spec/app/models/task_spec.rb
@@ -10,15 +10,36 @@ describe Task do
 		it { should respond_to( :title ) }
 		it { should respond_to( :estimated_time) }
 		it { should respond_to( :real_time ) }
-	it { should respond_to( :limit_date ) }
+		it { should respond_to( :limit_date ) }
 	end
 
-	describe 'set_title' do
+	describe 'All sets' do
 
-	  let(:task) { Task.new }
+		before do
+		 	@task = Task.new
+		 	@task.title = 'Tarea 1'
+		 	@task.estimated_time = 30
+		 	@task.real_time = 35
+		 	@task.limit_date = '2011-12-13'
+		end
 
-	  it '' do
-	  end
+		it 'should return title = Tarea 1' do
+			@task.title.should eq 'Tarea 1'
+		end
+
+		it 'should return estimated time = 30' do
+			@task.estimated_time.should eq 30
+		end
+
+		it 'should return real_time = 35' do
+			@task.real_time.should eq 35
+		end
+
+		it 'should return limit_date = 2011-12-13' do
+			@task.limit_date.day.should be 13
+			@task.limit_date.month.should be 12
+			@task.limit_date.year.should be 2011
+		end
 
 	end
 
